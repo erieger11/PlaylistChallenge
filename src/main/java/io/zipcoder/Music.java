609de2dfb60a6e3769418ddf3c1bb7110be64105
erieger11 <erieger530@gmail.com>
2024-03-15 21:03:41 -0400

    almost done

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,37 @@
 package io.zipcoder;
 
+import java.util.Objects;
+
 public class Music {
 
     private String[] playList;
+    private int counter1 = 0;
+    private int counter2 = 0;
 
     public Music(String[] playList){
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        for(int i = startIndex, length = playList.length; ; i = (i + 1) % length) {
+            if (!Objects.equals(playList[i], selection)){
+                counter1++;
+            }else if (playList[i].equals(selection)) {
+             break;
+            }
+        }
+        for(int i = startIndex, length = playList.length; ; i = (i + 1) % length) {
+            if (!Objects.equals(playList[i], selection)){
+                counter2++;
+            }else if (playList[i].equals(selection)) {
+                break;
+            }
+        }
+
+
+
+
+
+        return Math.min(counter1, counter2);
     }
 }
