0684824d51208af2827e90d617b9d1972efceff4
erieger11 <erieger530@gmail.com>
2024-03-15 23:22:44 -0400

    finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -5,33 +5,33 @@ import java.util.Objects;
 public class Music {
 
     private String[] playList;
-    private int counter1 = 0;
-    private int counter2 = 0;
 
-    public Music(String[] playList){
+
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        for(int i = startIndex, length = playList.length; ; i = (i + 1) % length) {
-            if (!Objects.equals(playList[i], selection)){
-                counter1++;
-            }else if (playList[i].equals(selection)) {
-             break;
+    public Integer selection(Integer startIndex, String selection) {
+        int counter1 = 0;
+        int counter2 = 0;
+        int n = playList.length;
+
+            for (int i = startIndex; ; i++) {
+                int index = i % n;
+                if (!Objects.equals(playList[index], selection)) {
+                    counter1++;
+                } else {
+                    break;
+                }
             }
-        }
-        for(int i = startIndex, length = playList.length; ; i = (i + 1) % length) {
-            if (!Objects.equals(playList[i], selection)){
-                counter2++;
-            }else if (playList[i].equals(selection)) {
-                break;
+            for (int j = startIndex; ; j--) {
+                int indexj = (j + n) % n;
+                if (!Objects.equals(playList[indexj], selection)) {
+                    counter2++;
+                } else {
+                    break;
+                }
             }
-        }
-
-
-
-
-
         return Math.min(counter1, counter2);
     }
 }
